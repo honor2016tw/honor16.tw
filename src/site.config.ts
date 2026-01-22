52889be7c8ca8f0b5002ed3ebdd4017e4c1040e7
Cheng-Ju Shih <honor2016tw@gmail.com>
2026-01-22 17:12:16 +0800

    fix: remove GitHub social link

--- src/site.config.ts
+++ src/site.config.ts
@@ -61,7 +61,7 @@ export const theme: ThemeUserConfig = {
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
     credits: false,
     /** Optional details about the social media accounts for this site. */
-    social: { github: 'https://github.com/honor2016tw' }
+    social: { }
   },
 
   // [Content]
